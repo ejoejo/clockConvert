2bdd414a957653933636044188c0a869614701b3
ejoejo <ejoejo@gmail.com>
2017-05-24 14:25:56 +0800

    change function return method

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ function clockConvert(millisecond) {
     var hh = Math.floor((millisecond / 1000) / 60 / 60);
     var mm = Math.floor((millisecond - hh * 1000 * 60 * 60) / 1000 / 60);
     var ss = Math.floor((millisecond - hh * 1000 * 60 * 60 - mm * 1000 * 60) / 1000);
-    console.log("轉換後：" + hh.toString() + ':' + mm.toString() + ':' + ss.toString());
+    return (hh.toString() + ':' + mm.toString() + ':' + ss.toString());
 }
-clockConvert(100000000);
+console.log("轉換後：" + clockConvert(100000000));

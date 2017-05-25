8446d4c855c1f919fdbbc7970adb9b4a971fb788
ejoejo <ejoejo@gmail.com>
2017-05-25 14:48:10 +0800

    change clock function & number convert string function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-function clockConvert(millisecond) {
-    var hh = Math.floor((millisecond / 1000) / 60 / 60);
-    var mm = Math.floor((millisecond - hh * 1000 * 60 * 60) / 1000 / 60);
-    var ss = Math.floor((millisecond - hh * 1000 * 60 * 60 - mm * 1000 * 60) / 1000);
-    return (hh.toString() + ':' + mm.toString() + ':' + ss.toString());
+function clock(millisecond) {
+    var hh = numConvert(Math.floor((millisecond / 1000) / 60 / 60));
+    var mm = numConvert(Math.floor((millisecond % (1000 * 60 * 60)) / 1000 / 60));
+    var ss = numConvert(Math.floor(millisecond % (1000 * 60 * 60) % (1000 * 60) / 1000));
+    return (hh + ':' + mm + ':' + ss);
 }
-console.log("轉換後：" + clockConvert(100000000));
+
+function numConvert(number) {
+    if (number === 0) {
+        return "00";
+    } else {
+        return number.toString();
+    }
+}
+console.log("轉換後：" + clock(100000000));
--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(function () {
     $(".btn").click(function () {
         var number = $("#input").val();
         var hh = Math.floor((number / 1000) / 60 / 60);
-        var mm = Math.floor((number - hh * 1000 * 60 * 60) / 1000 / 60);
-        var ss = Math.floor((number - hh * 1000 * 60 * 60 - mm * 1000 * 60) / 1000);
+        var mm = Math.floor((number % (1000 * 60 * 60)) / 1000 / 60);
+        var ss = Math.floor(number % (1000 * 60 * 60) % (1000 * 60) / 1000);
 
         $("#time").text(hh.toString() + ':' + mm.toString() + ":" + ss.toString());
     });
